f7d867f966355a7a96e54f9e998112207f8d0700
appleEmily <emiri3128@gmail.com>
2019-04-07 22:40:20 +0900

    3/6

--- calculater/ViewController.swift
+++ calculater/ViewController.swift
@@ -10,12 +10,37 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var result: UILabel!
+    
+    var number1: Int = 0
+    var number2: Int = 0
+    var number3: Int = 0
+    var ope: Int = 0
     
 
-    override func viewDidLoad() {
+    @IBAction func one() {
+        number1 = number1 * 10 + 1
+        result.text = String(number1)
+        
+    }
+    
+    
+    
+override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    
+    
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        print(result)
+        result.text = "hello"
+    }
+    
+    
 
 
 }
